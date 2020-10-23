176af63ed3586a8cd45a47527a9774b74edb6635
Makuku-888 <koch.marcus01@gmail.com>
2020-10-23 17:27:45 +0200

    mockCountry

---
+++ mockCountry.js
@@ -0,0 +1,14 @@
+const country = [{
+    id:1,
+    countryDescription: "Brazil",
+    contentType: "image/jpeg",
+    fileName: "brazil.jpg",
+    url: "//images.ctfassets.net/3evqbw19xpvd/2B1dNgnRyDv125ytsrRaG1/d830ef0bfcd962b5da234bd1b613e28a/brazil.jpg"
+}, {
+    id: 2,
+    countryDescription: "Nigeria",
+    contentType: "image/jpeg",
+    fileName: "Nigeria-national-arts-theatre.jpg",
+    url: "//images.ctfassets.net/3evqbw19xpvd/6gRT1HkV5v9lKVNSuKfhDd/3b1d1034fbd4f736e7bb816e797ae9db/Nigeria-national-arts-theatre.jpg"
+}]
+
